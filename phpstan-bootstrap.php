be72305a27cc0bd95795d55a6ebdbef49e7a1796
Mikel <mikel.basterrika@gmail.com>
2026-05-05 10:20:54 +0200

    Add PHPStan.

---
+++ phpstan-bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    define('ABSPATH', __DIR__ . '/../../../../');
+}
+
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    define('WP_UNINSTALL_PLUGIN', true);
+}
+
+if (!defined('WC_AC_PATH')) {
+    define('WC_AC_PATH', __DIR__ . '/');
+}
